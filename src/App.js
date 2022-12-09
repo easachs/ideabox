57311be0c2704dd6a9bbfc097034bc4c5738dc24
Eli Sachs <easachs13@gmail.com>
2022-12-08 23:16:33 -0700

    Update App for card and ideas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Ideas from './Ideas';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ideas: [
+        { id: 1, title: 'Run', description: 'Go on a 30 minute run' },
+        { id: 2, title: 'Clean', description: 'Your room is filthy' },
+        { id: 3, title: 'Meditate', description: 'Take a chill pill' },
+        { id: 4, title: 'Dentist', description: 'Get your teeth cleaned' },
+        { id: 5, title: 'Climb', description: 'Buy a climbing harness' },
+        { id: 6, title: 'React', description: 'Make an app with React' },
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <main className='App'>
+        <h1>Idea Box</h1>
+        <Ideas ideas={this.state.ideas} />
+      </main>
+    )
+  }
 }
 
 export default App;
